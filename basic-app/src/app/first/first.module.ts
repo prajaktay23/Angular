14bf4ffa6c28bf257b3f96bd4a5cb440fc7c9121
prajaktay23 <prajaktayadav525@gmail.com>
2019-12-02 17:53:05 +0530

    modules and components

---
+++ basic-app/src/app/first/first.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FirstSampleComponent } from '../first-sample/first-sample.component';
+
+
+
+@NgModule({
+  declarations: [FirstSampleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FirstModule { }
