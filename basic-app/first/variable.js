809efe2c4f6ddd527bc7687b239623638ef156f9
prajaktay23 <prajaktayadav525@gmail.com>
2019-12-13 19:06:18 +0530

    brush up : js

---
+++ basic-app/first/variable.js
@@ -0,0 +1,11 @@
+console.log('hi, welcome to codekul')
+
+var myNum = 'Android'
+var num = 10
+num = true
+
+let mob = 123
+if(true){
+    let mob = 456
+}
+console.log(mob)//if variable name is same it overrides the values of variable 
